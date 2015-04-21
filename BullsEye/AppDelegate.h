de0da83997cb985b53b21c16122d1758c18cefd1
rml596 <rml596@gmail.com>
2015-04-21 09:01:53 -0400

    Initial Commit

---
+++ BullsEye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BullsEye
+//
+//  Created by Robert Lynch on 4/21/15.
+//  Copyright (c) 2015 Robert Lynch Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
